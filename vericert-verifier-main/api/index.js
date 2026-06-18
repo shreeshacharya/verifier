c5066607f0b2965ccbeb51b6289b97c335b0ff31
shreeshacharya <shreesha.22cs145@sode-edu.in>
2026-06-18 21:07:08 +0530

    Refactor to Vercel Zero-Config setup

--- vericert-verifier-main/api/index.js
+++ vericert-verifier-main/api/index.js
@@ -92,9 +92,9 @@ if (process.env.NODE_ENV !== "production") {
     });
   });
 } else {
-  app.use(express.static(path.join(__dirname, "dist")));
+  app.use(express.static(path.join(__dirname, "../dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../dist", "index.html"));
   });
   
   // Only listen if not running on Vercel Serverless
